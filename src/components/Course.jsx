4afc908c9be9b7976c6931394d479836b7c21d95
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2025-08-14 01:24:05 +0530

    frontend

---
+++ src/components/Course.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Cards from "./Cards";
+import list from "../../public/list.json";
+import { Link } from "react-router-dom";
+
+function Course() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text:2xl md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500">Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam
+            dolore nobis perspiciatis consequatur aspernatur architecto deleniti
+            officia, sint eius beatae suscipit, atque necessitatibus dolorem!
+            Repudiandae natus architecto aperiam maxime fugiat! Voluptas
+            pariatur fuga debitis vel ullam veniam maiores distinctio assumenda!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-3">
+          {list.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
